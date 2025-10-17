c4f0ed47d5d58223dc486bcc9c61c4f535b6a5a5
Anderson Campos <158058420+AndersonCamposs@users.noreply.github.com>
2025-10-16 21:46:14 -0300

    Adicionando do back and

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public valorEntrada: number = 0;
+
+  public valorSaida: number = 0;
+
+  public totalCaixa: number = 0;
+
+  constructor() {
+
+  }
+
+  public adicionar(): void {
+    this.valorEntrada++;
+    this.obterTotalCaixa();
+  }
+
+  public retirar(): void {
+    this.valorSaida++;
+    this.obterTotalCaixa();
+  }
+
+  public obterTotalCaixa(): void {
+    this.totalCaixa = this.valorEntrada - this.valorSaida;
+  }
+
+  public verificarCor(): string {
+    if(this.totalCaixa < 0) {
+      return "danger";
+    } else {
+      return "primary";
+    }
+  }
 
 }
